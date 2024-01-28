0fda34a95c8031c764ba2d807243db0bc31f0594
hacki <work24to7@gmail.com>
2024-01-28 20:42:32 +0530

    Default Category Improved

--- app/Http/Controllers/registerController.php
+++ app/Http/Controllers/registerController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\TransactionCategory;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class registerController extends Controller
 {
@@ -17,9 +19,9 @@ class registerController extends Controller
      * @bodyParam email string required User email
      * @bodyParam password string required User password
      *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     * @throws \Illuminate\Validation\ValidationException
+     * @param Request $request
+     * @return Response
+     * @throws ValidationException
      */
     public function store(Request $request)
     {
@@ -36,11 +38,19 @@ class registerController extends Controller
             'password' => bcrypt($request->password)
         ]);
 
-        DB::table('transaction_categories')->insert(
-            collect(TransactionCategory::$DEFAULT_CATEGORIES)->map(function ($item) use ($user) {
-                return array_merge($item, ['created_by' => $user->id]);
-            })->toArray()
-        );
+        DB::table('transaction_categories')->insert([
+            ['category_name' => 'Utilities', 'category_type' => 'Expense', 'created_by' => $user->id, 'updated_by' => $user->id],
+            ['category_name' => 'Groceries', 'category_type' => 'Expense', 'created_by' => $user->id, 'updated_by' => $user->id],
+            ['category_name' => 'Entertainment', 'category_type' => 'Expense', 'created_by' => $user->id, 'updated_by' => $user->id],
+            ['category_name' => 'Healthcare', 'category_type' => 'Expense', 'created_by' => $user->id, 'updated_by' => $user->id],
+            ['category_name' => 'Transportation', 'category_type' => 'Expense', 'created_by' => $user->id, 'updated_by' => $user->id],
+            ['category_name' => 'Other Expenses', 'category_type' => 'Expense', 'created_by' => $user->id, 'updated_by' => $user->id],
+            ['category_name' => 'Salary', 'category_type' => 'Income', 'created_by' => $user->id, 'updated_by' => $user->id],
+            ['category_name' => 'Freelance Income', 'category_type' => 'Income', 'created_by' => $user->id, 'updated_by' => $user->id],
+            ['category_name' => 'Investment Returns', 'category_type' => 'Income', 'created_by' => $user->id, 'updated_by' => $user->id],
+            ['category_name' => 'Gifts Received', 'category_type' => 'Income', 'created_by' => $user->id, 'updated_by' => $user->id],
+            ['category_name' => 'Other Income', 'category_type' => 'Income', 'created_by' => $user->id, 'updated_by' => $user->id],
+        ]);
         return response(['data' => 'registration_successful'], 201);
     }
 }
